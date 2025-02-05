6e363f709309ef7e1ccf6d90ed64ac12ed674e96
James Miller <JMTNTBANG@proton.me>
2025-02-04 21:42:57 -0600

    Updated to MySQL2 NPM Package

--- src/requests/post/auth.js
+++ src/requests/post/auth.js
@@ -6,30 +6,34 @@ module.exports = {
       const username = request.body.username;
       const password = request.body.password;
       console.log(`Authenticating ${request.ip} with Username ${username}`);
-      const database = func.connectToMySQL();
-      database.query(
-        "SELECT * FROM auth.accounts WHERE username = ? AND password = ?",
-        [username, password],
-        (err, results, fields) => {
-          if (err) throw err;
-          if (results.length > 0) {
-            console.log(
-              `Successfully Authenticated ${request.ip} with Username ${username}`
-            );
-            request.session.vgc = {
-              authenticated: true,
-              username: username,
-              userid: results[0].id,
-            };
-            response.redirect(prefix);
-          } else {
-            console.error(
-              `Failed to Authenticate ${request.ip} with Username ${username}`
-            );
-            response.send(func.sendError("Incorrect Username and/or Password"));
+      func.connectToMySQL(response, (err, db) => {
+        if (err) throw err;
+        db.query(
+          "SELECT * FROM auth.accounts WHERE username = ? AND password = ?",
+          [username, password],
+          (err, results, fields) => {
+            if (err) throw err;
+            if (results.length > 0) {
+              console.log(
+                `Successfully Authenticated ${request.ip} with Username ${username}`
+              );
+              request.session.vgc = {
+                authenticated: true,
+                username: username,
+                userid: results[0].id,
+              };
+              response.redirect(prefix);
+            } else {
+              console.error(
+                `Failed to Authenticate ${request.ip} with Username ${username}`
+              );
+              response.send(
+                func.sendError("Incorrect Username and/or Password")
+              );
+            }
           }
-        }
-      );
+        );
+      });
     });
   },
 };
