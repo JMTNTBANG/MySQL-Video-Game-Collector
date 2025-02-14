fc7ea3989494faa8c819fa120f961b9723af6506
James Miller <JMTNTBANG@proton.me>
2025-02-14 14:57:17 -0600

    v0.9.4 - Authentication Checks on any Request - #4

--- src/requests/get/getItems.js
+++ src/requests/get/getItems.js
@@ -3,16 +3,20 @@ const func = require("../../static/func");
 module.exports = {
   init: (prefix, website) => {
     website.get(`${prefix}getItems`, (request, response) => {
-      func.connectToMySQL(response, (err, db) => {
-        if (err) throw err;
-        db.query(
-          "SELECT * FROM `Gaming`.`Entries`",
-          (err, result, fields) => {
-            if (err) throw err;
-            response.send(result)
-          }
-        );
-      });
+      if (!request.session.vgc || !request.session.vgc.authenticated) {
+        response.redirect(`${prefix}`);
+      } else {
+        func.connectToMySQL(response, (err, db) => {
+          if (err) throw err;
+          db.query(
+            "SELECT * FROM `Gaming`.`Entries`",
+            (err, result, fields) => {
+              if (err) throw err;
+              response.send(result);
+            }
+          );
+        });
+      }
     });
   },
 };
